25c5b6fc607c480981ca4da0f2c58972e84feac0
Abhijit Upadhyay <abhijitupadhyay03@gmail.com>
2025-07-20 19:48:45 +1000

    Fix navbar background and z index

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,11 +2,11 @@ import Link from "next/link"
 
 const Navbar = () => {
   return (
-    <nav className="fixed top-0 w-full border-b border-gray-100">
+    <nav className="fixed top-0 w-full border-b border-gray-100 bg-white/80 backdrop:blur-md z-50">
       <div className="p-4 container mx-auto flex justify-between items-center">
         <Name />
         {/* TODO: make resposive */}
-        <ul className="flex items-center gap-8 text-gray-600">
+        <ul className="hidden md:flex items-center gap-8 text-gray-600">
           <li className="hover:text-gray-900 transition-colors">
             <Link href="#hero">Home</Link>
           </li>  
